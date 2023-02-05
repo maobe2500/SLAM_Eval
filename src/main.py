2ec3b2af7c73c73131d859cf6023cfd967dd7301
Stenmarken2 <Victor.stenmark22@gmail.com>
2023-02-05 14:24:09 +0100

    Fix functions for real cone data

--- src/main.py
+++ src/main.py
@@ -61,16 +61,19 @@ def read_real_yaml_bag(path):
     with open(path) as f:
         map_data = {}
         maps = list(yaml.load_all(f, yaml.Loader))
+        maps_size = len(maps)
+        counter = 0
         # Needed try/except since we end with Ctrl-C which cuts data off uncleanly at the end
         try:
             for slam_map in maps:
+                counter += 1
                 time_stamp = slam_map["header"]["seq"]
                 mapped_cones = slam_map["cones"]
                 map_data[time_stamp] = []
                 for i, cone_with_stats in enumerate(mapped_cones):
                     x = cone_with_stats["x"]
                     y = cone_with_stats["y"]
-                    covariance = cone["covariance"]
+                    covariance = cone_with_stats["covariance"]
                     map_data[time_stamp].append({"time_stamp": time_stamp, "x": x, "y": y, "covariance": covariance, "id": i})
         except Exception as e:
             print("Whoops", e)
@@ -128,7 +131,6 @@ def write_to(filename, data, fieldnames=None):
 
     :return: None
     """
-    print("First key : ", list(data.keys())[0])
 
     filetype = filename.split(".")[1]
     if filetype != "csv" and filetype != "json":
@@ -193,6 +195,42 @@ def plot_real_cones(axes_limits=25):
     ax.scatter(yx, yy, c="yellow")
     return fig, ax
 
+
+def read_map_csv_real_data(path, animate_maps = False):
+    """
+    Reads the csv file created by the read_yaml_bag function.
+
+    :param path:            Path to the csv file
+    :param animate_maps:    If set to true: gives back a list of maps, where each map is a 
+                            map is a list of (x, y) tuples for a given timestamp.
+
+    :return (x_vals, y_vals, times):    tuple containing a all x and y values with times stamps
+                                        The time stamp is what signifies what
+
+    :TODO:  Make readable by humans. Current state is due to bugfixing at 3:am.
+
+    """
+    with open(path) as f:
+        rows = csv.reader(f)
+        field_names = next(rows)
+        map_frames = []
+        times, x_vals, y_vals = [], [], []
+        x, y, cov = [], [], []
+        for i, row in enumerate(rows):  # Every row contains data for a single cone
+            times.append(int(row[0]))
+            x_vals.append(float(row[1]))
+            x.append(float(row[1]))
+            y.append(float(row[2]))
+            y_vals.append(float(row[2]))
+            # --- Not using these right now ---
+            cov.append(([float(var) for var in row[3].split(",")[1:-2]]))
+            #----------------------------------
+            if animate_maps and int(row[4]) == 0 and i != 0:
+                map_frames.append([x_vals, y_vals, cov])
+                x_vals, y_vals = [], []
+        if animate_maps:
+            return map_frames
+        return x, y, times
 
 def read_map_csv(path, animate_maps = False):
     """
@@ -300,24 +338,39 @@ class MapAnimation:
                 filename = datetime.datetime.now().strftime("map_at_time_%m-%d-%H:%M.gif")
                 ani.save(filename, writer=PillowWriter(fps=30))
 
-
-def main():
+def run_simulated_cone_data():
     matplotlib.style.use("ggplot")
     # Change this to the path of the bag you want to read
-    map_data = read_real_yaml_bag(path="bags/real_cone_data.yaml")
-    # print(map_data)
+    map_data = read_yaml_bag(path="bags/mapped_cone_data.yaml")
+
     # Change this to the path of the csv file you want to write to
-    write_to("csv_files/real_cone_data.csv", map_data)
+    write_to("csv_files/mapped_cones_data.csv", map_data)
     fig, ax = plot_real_cones()
     # Link to all colormaps: https://matplotlib.org/2.0.2/examples/color/colormaps_reference.html
     # To use a colormap, add "_r" to the end of the name
     #x_vals, y_vals, times = read_map_csv("./csv_files/mapped_cones_data.csv")
     #ax.scatter(x_vals, y_vals, s=20, alpha=0.1, c=inn_std, cmap="cool_r")
     #plt.show()
-    map_frames = read_map_csv("csv_files/real_cone_data.csv", animate_maps=True)
+    map_frames = read_map_csv("csv_files/mapped_cones_data.csv", animate_maps=True)
     ani = MapAnimation(map_frames)
     ani.animate()
 
+
+def run_real_cone_data():
+    matplotlib.style.use("ggplot")
+    # Change this to the path of the bag you want to read
+    map_data = read_real_yaml_bag(path="bags/real_cone_data.yaml")
+
+    # Change this to the path of the csv file you want to write to
+    write_to("csv_files/real_cone_data.csv", map_data)
+    map_frames = read_map_csv_real_data("csv_files/real_cone_data.csv", animate_maps=True)
+    ani = MapAnimation(map_frames)
+    ani.animate()
+
+
+def main():
+    run_real_cone_data()
+    #run_simulated_cone_data()
 
 
 if __name__ == '__main__':
